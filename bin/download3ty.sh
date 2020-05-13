70a34b8edf8b527cde226bf87615daa81c4d8df5
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-05-13 16:58:07 +0200

    3ty updated

--- bin/download3ty.sh
+++ bin/download3ty.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p 3ty/ && cd 3ty
+
 if [[ ! -d "proc-comm-lib-ows-1.0" ]]
 then
   curl -L  "https://github.com/EOEPCA/proc-comm-lib-ows/archive/v1.0.tar.gz" -o proc-comm-lib-ows-v1.0.tar.gz
